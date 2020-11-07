889c3e8b55e6877372af5b9a537940a7c43afa30
Jasmine Bailey <lajoye.russell@gmail.com>
2020-11-07 06:51:41 -0500

    redid hw

---
+++ js/questions.js
@@ -0,0 +1,23 @@
+// list of all questions, choices, and answers
+var questions = [
+  {
+    title: "How many fire signs are there in the Zodiac Calendar?",
+    choices: ["four","five","three","two"],
+    answer: "three"
+  },
+  {
+    title: "What sign has a centaur?",
+    choices: ["aries", "sagittarius","cancer","pisces"],
+    answer:  "sagittarius"
+  },
+  {
+    title: "How many  water signs are in the zodiac Calendar?",
+    choices:  ["four","five","three","two"],
+    answer: 'three'
+  },
+  {
+    title: "How many  signs does the Chinese Zodia have?",
+    choices:  ["eleven","eight","ten","twelve"],
+    answer: 'twelve'
+  }
+];
